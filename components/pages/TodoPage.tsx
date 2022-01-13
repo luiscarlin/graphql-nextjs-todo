a74993d3acebc3e5c095d353a1acd3876ad32ed9
Luis Carlin <luiscarlin@gmail.com>
2022-01-13 10:17:12 -0500

    setup graphql

--- components/pages/TodoPage.tsx
+++ components/pages/TodoPage.tsx
@@ -1,5 +1,49 @@
+import { gql, useQuery } from "@apollo/client";
+
+type Todo = {
+  id: string;
+  title: string;
+  user: {
+    id: string;
+    name: string;
+  };
+};
+
+type GetAllTodos = {
+  todos: [Todo];
+};
+
+const GET_ALL_TODOS = gql`
+  query {
+    todos {
+      id
+      title
+      user {
+        id
+        name
+      }
+    }
+  }
+`;
+
 const TodoPage = () => {
-  return <>Here are all the todos</>;
+  const { data, loading, error } = useQuery<GetAllTodos>(GET_ALL_TODOS);
+
+  if (loading) {
+    return <>loading...</>;
+  }
+
+  if (error) {
+    return <>there was an error</>;
+  }
+
+  return (
+    <div>
+      {data?.todos.map((todo) => {
+        return <div key={todo.id}>{todo.title}</div>;
+      })}
+    </div>
+  );
 };
 
 export default TodoPage;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ApolloProvider } from "@apollo/client";
+import type { AppProps } from "next/app";
+import client from "../apollo-client";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
 
-export default MyApp
+export default App;
